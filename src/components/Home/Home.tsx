207719ac7f36eb44705a14b74889f5f6fcfd0622
Sam <smc397@uclive.ac.nz>
2024-05-15 17:14:07 +1200

    image slider on home page started

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,11 +3,16 @@ import './Home.css';
 import axios from "axios";
 import { original } from "../../types/original";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
+
+
 
 const Home: React.FC = () => {
 
   const [orginals, setOrginals] = React.useState<original[]>([]);
-  const [slide, setSlide] = useState(2);
+  const [slide, setSlide] = useState(0);
 
   useEffect(() => {
     axios.get('http://localhost:8000/paintings/originals').then((response) => {
@@ -18,18 +23,22 @@ const Home: React.FC = () => {
     });
   }, []);
 
+  function showPrevImage() {
+    setSlide(index => {
+      if (index === 0) return orginals.length - 1
+      return index - 1
+    })
+  }
+
+  function showNextImage() {
+    setSlide(index => {
+      if (index === orginals.length - 1) return 0
+      return index + 1
+    })
+  }
+
     return (
       <div className="home-div">
-          <div className="originals-div">
-            <h1>Latest artwork</h1>
-            <div className="originals-carousel">
-            {orginals.map((orginal: original, index: number) => (
-                  <NavLink className="originals-image-link" to={`/Originals/${orginal.id}`}>
-                    <img className={slide === index ? "slide" : "slide slide-hidden"} src={"./src/mocks/images/" + orginal.title + ".jpg"} alt={orginal.title} key={index} />
-                  </NavLink>
-             ))}
-            </div>
-          </div>
 
         <div className="expression-div">
           <img className="quote-image" src={"/src/assets/quote.jpg"} alt='quote' />
@@ -38,6 +47,40 @@ const Home: React.FC = () => {
             which can bring a painting to life. The interplay of colors, shadows, and contrasts illuminated by this type of light can transform even the most mundane subject into something truly captivating."</h3>
           </div>
         </div>
+
+        <hr></hr>
+
+        <div className="originals-div">
+          <NavLink className="originals-link" to={'/Originals'}>
+            <h1>Latest Originals</h1>
+          </NavLink>
+
+          
+          <div className="originals-carousel">
+
+            <button  onClick={showPrevImage} className="img-slider-btn" style={{left : 0}}>
+            <div className="test-button">
+              <FontAwesomeIcon icon={faArrowLeft} size="2x"/>
+              </div>
+            </button>
+
+            {orginals.map((orginal: original, index: number) => (
+                  <NavLink className="originals-image-link" to={`/Originals/${orginal.id}`}>
+                    <img className={slide === index ? "slide" : "slide slide-hidden"} src={"./src/mocks/images/" + orginal.title + ".jpg"} alt={orginal.title} key={index} />
+                  </NavLink>
+              ))}
+              <button onClick={showNextImage} className="img-slider-btn" style={{right : 0}}>
+                <div className="test-button">
+                <FontAwesomeIcon icon={faArrowRight} size="2x" />
+                </div>
+              </button>
+          </div>
+
+
+
+        </div>
+
+        <hr></hr>
 
         <div className="prints-div">
           <h1>Fine Art Prints</h1>
